1499840d3317d9ba9b63b6e60a38cf28d07d0fd8
pcw0611 <pcw0611@gmail.com>
2021-11-09 13:53:15 +0900

    no message

---
+++ DesignPatterns/Command@C#/Robot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Robot
+{
+	public enum Direction
+	{
+		Left,
+		Right,
+	}
+
+	public void MoveForward( int space )
+	{
+		Console.WriteLine( space + " 칸 전진" );
+	}
+	public void Turn( Direction direction )
+	{
+		Console.WriteLine( ( direction == Direction.Left ? "왼쪽" : "오른쪽" ) + "으로 방향 전환" );
+	}
+	public void Pickup()
+	{
+		Console.WriteLine( "앞의 물건 집어들기" );
+	}
+}
