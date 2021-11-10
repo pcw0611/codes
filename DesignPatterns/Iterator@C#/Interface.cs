93577170845c8544872b9ca1a62644b787168f08
pcw0611 <pcw0611@gmail.com>
2021-11-10 14:41:50 +0900

    no message

---
+++ DesignPatterns/Iterator@C#/Interface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+interface IContainer
+{
+	int Count { get; }
+
+	bool Add( Object obj );
+	void Remove( Object obj );
+	void Clear();
+	IIterator MakerIterator();
+}
+
+public interface IIterator
+{
+	Object Current { get; }
+
+	void Begin();
+	bool MoveNext();
+}
---
+++ DesignPatterns/Iterator@C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program 
+{
+	static void Main( string[] args )
+	{
+		Arr arr = new Arr( 10 );
+		arr.Add( 7 );
+		arr.Add( 8 );
+		arr.Add( 9 );
+		IIterator iter = arr.MakerIterator();
+
+		iter.Begin();
+		while( iter.MoveNext() )
+		{
+			Console.WriteLine( iter.Current );
+		}
+	}
+}
