a92aecdccd45ef3e10ed3e866ee037f834f14d01
pcw0611 <pcw0611@gmail.com>
2021-11-10 20:44:33 +0900

    no message

---
+++ DesignPatterns/Observer@C#/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Inventory
+{
+	List<IEquipObserver> equipObservers = new List<IEquipObserver>();
+
+	public void Equip( Item item )
+	{
+		// Notify
+		foreach ( IEquipObserver observer in equipObservers )
+		{
+			observer.OnEquip( item );
+		}
+	}
+
+	public void AddEquipObserver( IEquipObserver equipObserver )
+	{
+		equipObservers.Add( equipObserver );
+	}
+}
