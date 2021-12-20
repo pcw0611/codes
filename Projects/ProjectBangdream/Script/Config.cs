9bff1bdc74df3a278d312601e413c5b551cb4cb0
pcw0611 <pcw0611@gmail.com>
2021-12-20 21:44:30 +0900

    no message

---
+++ Projects/ProjectBangdream/Script/Config.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour
+{
+	[SerializeField] public float bpm;
+	[SerializeField] public float goalTime;
+	[SerializeField] public float correctionDist;
+}
